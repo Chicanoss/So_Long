caf3b0d2e994332628031d848a83142fbe8b18ae
Robin Geral <rgeral@z4r11p7.42lyon.fr>
2022-02-17 17:06:26 +0100

    2nd commit

--- incs/fdf.h
+++ incs/fdf.h
@@ -6,7 +6,7 @@
 /*   By: rgeral <rgeral@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 21:19:13 by rgeral            #+#    #+#             */
-/*   Updated: 2022/02/16 16:16:28 by rgeral           ###   ########.fr       */
+/*   Updated: 2022/02/17 13:11:15 by rgeral           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 # include "../minilibx_macos/mlx.h"
 # include <unistd.h>
 # include <stdio.h>
+# include <math.h>
 
 # endif
--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,12 +6,33 @@
 /*   By: rgeral <rgeral@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 21:19:10 by rgeral            #+#    #+#             */
-/*   Updated: 2022/02/16 18:57:01 by rgeral           ###   ########.fr       */
+/*   Updated: 2022/02/17 13:18:29 by rgeral           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minilibx_macos/mlx.h"
 #include "../incs/fdf.h"
+
+int	draw_line (void *mlx, void *win, int beginX, int beginY, int endX, int endY, int color)
+{
+	double deltaX = endX - beginX; // 10
+	double deltaY = endY - beginY; // 0
+
+	int pixels = sqrt((deltaX * deltaX) + (deltaY * deltaY));
+	deltaX /= pixels; // 1
+	deltaY /= pixels; // 0
+
+	double pixelX = beginX;
+	double pixelY = beginY;
+	while (pixels)
+	{
+   		mlx_pixel_put(mlx, win, pixelX, pixelY, color);
+    	pixelX += deltaX;
+    	pixelY += deltaY;
+    	--pixels;
+	}
+	return(1);
+}
 
 int	main(void)
 {
@@ -24,12 +45,14 @@ int	main(void)
 	y = 0;
 	color = 0;
 	id = mlx_init();
-	win = mlx_new_window(id, 1000, 1000, "Hello");
+	win = mlx_new_window(id, 300, 300, "Hello");
 	if (!id)
 		printf("noob\n");
 	y = 0;
 	color = 0;
-	mlx_pixel_put (id, win, x, y, color);
+	//mlx_pixel_put (id, win, x, y, color);
+	draw_line(id, win, 0, 0, 300, 300, 0xFFFFFF);
+	draw_line(id, win, 300, 0, 0, 300, 0xFFFFFF);
 
 	mlx_loop(id);
 	return(1);
