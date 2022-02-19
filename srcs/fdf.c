2733a6589cd7f02cc3223a7f73c483a11a1d8d8d
Robin Geral <robingeral@MacBook-Air-de-Robin.local>
2022-02-19 22:50:19 +0100

    WIP make_map

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,7 +6,7 @@
 /*   By: rgeral <rgeral@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 21:19:10 by rgeral            #+#    #+#             */
-/*   Updated: 2022/02/19 20:12:31 by rgeral           ###   ########.fr       */
+/*   Updated: 2022/02/19 22:49:49 by rgeral           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,58 +34,52 @@ int	draw_line (void *mlx, void *win, int beginX, int beginY, int endX, int endY,
 	return(1);
 }
 
+int	**make_map(fd)
+{
+	int		i;
+	int		j;
+	char	**splited;
+	char	*temp;
+	int		**map;
+	
+	i = 0;
+	j = 0;
+	while(i < 9)
+	{
+		temp = get_next_line(fd);
+		splited = ft_split(temp , ' ');
+		map = put_in_map(splited);
+	}
+	return (map);
+}
+
+int	**put_in_map(char	**splited)
+{
+	int	**map;
+	int i;
+
+	
+
+}
+
 int	main(void)
 {
-	void *id;
-	void *win;
-	int color;
-
-	color = 253;
-	id = mlx_init();
-	win = mlx_new_window(id, 300, 300, "Hello");
-	if (!id)
-		printf("noob\n");
-	color = 0;
-	//mlx_pixel_put (id, win, 10, 10, color);
-	//draw_line(id, win, 0, 0, 300, 300, 0xFFFFFF);
-	//draw_line(id, win, 300, 0, 0, 300, 0xFFFFFF);
-
-	int fd;
-	char *rslt = NULL;
-	char	**splited;
+	int	**map;
+	int	fd;
 	int i;
-	int x;
-	int	y;
-	int	j;
-	int	space;
 
-	i = 0;
-	x = 1;
-	y = 1;
-	j = 0;
-	space = 10;
+	/*parsing 
+	if (!checkmap)
+		return (error);
+	*/
 	fd = open("srcs/10-2.fdf" , O_RDWR);
-	while(i < 10)
-	{
-		rslt = get_next_line(fd);
-		//printf("test");
-		splited = ft_split(rslt, ' ');
-		while(splited[j])
-		{
-			//printf("test");
-			printf("coord : [%d,%d]\n valeur : %s", x, y, splited[j]);
-			mlx_pixel_put (id, win, x, y, 0xFFFFFF);
-			x += space * 2 + 1;
-			j++;
-		}
-		x = 0;
-		y += space + 1;
-		j = 0;
-		i++;
-	}
-
-	//printf("%s\n", rslt);
-	//printf("%s\n", rslt);
-	mlx_loop(id);
-	return(1);
+	map = make_map(fd);
+	i = 0;
+	//while (map)
+	//{
+		//printf("%d\n" , map[3][1]);
+		//i++;
+	//}
+	
+	return (1);
 }
